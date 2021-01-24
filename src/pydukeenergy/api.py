d9b5274247cb8d8b59a4683d4a6095d16e57b2a3
Dayz <k.sienkiewicz@gmail.com>
2021-01-24 11:52:49 -0500

    Download My Data instead of pulling Chart

--- src/pydukeenergy/api.py
+++ src/pydukeenergy/api.py
@@ -3,17 +3,18 @@ import json
 import sys
 from datetime import datetime, timedelta
 
-from bs4 import BeautifulSoup
 import requests
 
 from pydukeenergy.meter import Meter
 
 BASE_URL = "https://www.duke-energy.com/"
-LOGIN_URL = BASE_URL + "form/Login/GetAccountValidationMessage"
+LOGIN_URL = BASE_URL + "form/SignIn/GetAccountValidationMessage"
+# LOGIN_URL = BASE_URL + "form/Login/GetAccountValidationMessage"
 USAGE_ANALYSIS_URL = BASE_URL + "api/UsageAnalysis/"
 BILLING_INFORMATION_URL = USAGE_ANALYSIS_URL + "GetBillingInformation"
 METER_ACTIVE_URL = BASE_URL + "my-account/usage-analysis"
 USAGE_CHART_URL = USAGE_ANALYSIS_URL + "GetUsageChartData"
+DOWNLOAD_DATA_URL = USAGE_ANALYSIS_URL + "DownloadMyData"
 
 USER_AGENT = {"User-Agent": "python/{}.{} pyduke-energy/0.0.6"}
 LOGIN_HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
@@ -27,12 +28,14 @@ class DukeEnergy(object):
     API interface object.
     """
 
-    def __init__(self, email, password, update_interval=60):
+    def __init__(self, email, password, electric_meters, gas_meters=None, update_interval=60, verify_ssl=False):
         """
         Create the Duke Energy API interface object.
         Args:
             email (str): Duke Energy account email address.
             password (str): Duke Energy account password.
+            electric_meters (list of str): List of electric meter ID's to monitor
+            gas_meters (list of str): List of gas meter ID's to monitor
             update_interval (int): How often an update should occur. (Min=10)
         """
         global USER_AGENT
@@ -42,6 +45,19 @@ class DukeEnergy(object):
         USER_AGENT["User-Agent"] = USER_AGENT["User-Agent"].format(major, minor)
         self.email = email
         self.password = password
+        self.verify = verify_ssl
+        if self.verify is False:
+            _LOGGER.warning("User has chosen to disable SSL verification. Supressing all insecure request warnings.")
+            import urllib3
+            urllib3.disable_warnings()
+        if type(electric_meters) is not list:
+            electric_meters = list([electric_meters])
+        if not gas_meters:
+            self._meters = {"ELECTRIC": electric_meters, "GAS": []}
+        else:
+            if type(gas_meters) is not list:
+                gas_meters = list([gas_meters])
+            self._meters = {"ELECTRIC": electric_meters, "GAS": gas_meters}
         self.meters = []
         self.session = requests.Session()
         self.update_interval = update_interval
@@ -57,11 +73,11 @@ class DukeEnergy(object):
         Pull the billing info for the meter.
         """
         if self.session.cookies or self._login():
-            post_body = {"MeterNumber": meter.type + " - " + meter.id}
+            post_body = {"MeterNumber": f"{meter.type} - {meter.id}"}
             headers = USAGE_ANALYSIS_HEADERS.copy()
             headers.update(USER_AGENT)
             response = self.session.post(BILLING_INFORMATION_URL, data=json.dumps(post_body), headers=headers,
-                                         timeout=10)
+                                         timeout=10, verify=self.verify)
             _LOGGER.debug(str(response.content))
             try:
                 if response.status_code != 200:
@@ -92,34 +108,43 @@ class DukeEnergy(object):
             the_date = meter.date - timedelta(days=1)
         else:
             the_date = meter.date
+        #~ if self.session.cookies or self._login():
+            #~ post_body = {
+                #~ "Graph": "DailyEnergy",
+                #~ "BillingFrequency": "Billing Cycle",
+                #~ "GraphText": "Daily Energy and Avg. ",
+                #~ "Date": meter.start_date,
+                #~ "MeterNumber": meter.type + " - " + meter.id,
+                #~ "ActiveDate": "11/18/2019"
+            #~ }
+            #~ headers = USAGE_ANALYSIS_HEADERS.copy()
+            #~ headers.update(USER_AGENT)
+            #~ response = self.session.post(USAGE_CHART_URL, data=json.dumps(post_body), headers=headers, 
+                                        #~ timeout=10, verify=self.verify)
+            #~ _LOGGER.debug(str(response.content))
+            
         if self.session.cookies or self._login():
             post_body = {
-                "Graph": "DailyEnergy",
-                "BillingFrequency": "Week",
-                "GraphText": "Daily Energy and Avg. ",
-                "Date": the_date.strftime("%m / %d / %Y"),
                 "MeterNumber": meter.type + " - " + meter.id,
-                "ActiveDate": meter.start_date
             }
+            print (post_body)
             headers = USAGE_ANALYSIS_HEADERS.copy()
             headers.update(USER_AGENT)
-            response = self.session.post(USAGE_CHART_URL, data=json.dumps(post_body), headers=headers, timeout=10)
+            response = self.session.post(DOWNLOAD_DATA_URL, data=json.dumps(post_body), headers=headers, 
+                                        timeout=10, verify=self.verify)
             _LOGGER.debug(str(response.content))
+
+
             try:
                 if response.status_code != 200:
                     _LOGGER.error("Usage data request failed: " + response.status_code)
                     self._logout()
                     return False
-                if response.json()["Status"] == "ERROR":
-                    self._logout()
-                    return False
-                if response.json()["Status"] == "OK":
-                    meter.set_chart_usage(response.json())
-                    return True
                 else:
-                    self._logout()
-                    return False
+                    open('data.xml', 'w').write(response.text)
+                    return True
             except Exception as e:
+                print ("dsg")
                 _LOGGER.exception("Something went wrong. Logging out and trying again.")
                 self._logout()
                 return False
@@ -134,9 +159,14 @@ class DukeEnergy(object):
         data = {"userId": self.email, "userPassword": self.password, "deviceprofile": "mobile"}
         headers = LOGIN_HEADERS.copy()
         headers.update(USER_AGENT)
-        response = self.session.post(LOGIN_URL, data=data, headers=headers, timeout=10)
+        try:
+            response = self.session.post(LOGIN_URL, data=data, headers=headers, timeout=10, verify=self.verify)
+        except requests.exceptions.SSLError:
+            _LOGGER.error("SSL certificate error. Trying setting 'verify' to False.")
+        except Exception:
+            _LOGGER.exception("Failed to log in")
         if response.status_code != 200:
-            _LOGGER.error("Failed to log in")
+            _LOGGER.exception("Failed to log in")
             return False
         response = self.session.get(METER_ACTIVE_URL, timeout=10)
         return True
@@ -153,19 +183,15 @@ class DukeEnergy(object):
         There doesn't appear to be a service to get this data.
         Collecting the meter info to build meter objects.
         """
+        
         if self._login():
-            response = self.session.get(METER_ACTIVE_URL, timeout=10)
-            _LOGGER.debug(str(response.text))
-            soup = BeautifulSoup(response.text, "html.parser")
-            meter_data = json.loads(soup.find("duke-dropdown", {"id": "usageAnalysisMeter"})["items"])
-            _LOGGER.debug(str(meter_data))
-            for meter in meter_data:
-                meter_type, meter_id = meter["text"].split(" - ")
-                meter_start_date = meter["CalendarStartDate"]
-                self.meters.append(Meter(self, meter_type, meter_id, meter_start_date, self.update_interval))
+            for meter in self._meters['ELECTRIC']:
+                self.meters.append(Meter(self, "ELECTRIC", str(meter), self.update_interval))
+            if len(self._meters['GAS']) != 0:
+                for meter in self._meters['GAS']:
+                    self.meters.append(Meter(self, "GAS", str(meter), self.update_interval))
             self._logout()
 
 
 class DukeEnergyException(Exception):
     pass
-
